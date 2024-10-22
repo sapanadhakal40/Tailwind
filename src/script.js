ddfb1eb219008c9838e46f2b83718d6c3eded3dc
sapanadhakal40 <sapna.dhakal40@gmail.com>
2024-10-22 14:05:12 +0300

    Add variable color

--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@
 // Select the toggle button and menu elements
 const toggleButton = document.getElementById('nav-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
 
 // Toggle the menu on click
 toggleButton.addEventListener('click', () => {
@@ -17,3 +20,60 @@ productsToggleMobile.addEventListener('click', () => {
     // e.preventDefault();
     mobileProductsDropdown.classList.toggle('hidden');
 });
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun'); // Switch to sun icon for light mode
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon'); // Switch to moon icon for dark mode
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// themeToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('dark');
+
+//     if (document.body.classList.contains('dark')) {
+//         themeToggle.innerText = "Light Mode";  // Change button text to "Light Mode"
+//     } else {
+//         themeToggle.innerText = "Dark Mode";   // Change button text to "Dark Mode"
+//     }
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
+    extend: {
+      colors: {
+        custom: 'var(--text-color)',
+        customBg: 'var(--background-color)',
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        'white': '#ffffff',
+        'black': '#000000',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
     screens: {
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
-    },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'white': '#ffffff',
-      'black': '#000000',
-    },
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
     },
   },
   plugins: [],
