fc5074c49bf009ee930fda13eb6ad637ebc092b8
sapanadhakal40 <sapna.dhakal40@gmail.com>
2024-11-19 11:11:06 +0200

    Add custom blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         custom: 'var(--text-color)',
         customBg: 'var(--background-color)',
         'blue': '#1fb6ff',
+        'blue-dark': '#1a73e8',
         'purple': '#7e5bef',
         'pink': '#ff49db',
         'orange': '#ff7849',
@@ -18,6 +19,7 @@ module.exports = {
         'gray-light': '#d3dce6',
         'white': '#ffffff',
         'black': '#000000',
+        'red': '#ef4444'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
